8532387ee9d85321a52e90dd15015a60580b17f1
LEZHONG-iMAC <lezhong-imac@LEZHONG-iMACdeiMac.local>
2017-04-26 16:29:51 +0800

    定义两个变量,实现他们的相加

--- KouJiaoLaoHan/ViewController.m
+++ KouJiaoLaoHan/ViewController.m
@@ -10,6 +10,8 @@
 
 @interface ViewController ()
 
+@property (nonatomic) int sum;
+
 @end
 
 @implementation ViewController
@@ -18,6 +20,12 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"Hello Word!!!");
+    int a = 5;
+    int b = 10;
+    
+    self.sum = a + b;
+    
+    NSLog(@"The result is: %d", self.sum);
 }
 
 
