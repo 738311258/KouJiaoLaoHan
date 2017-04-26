2997836cc343ad443d108a1c498bb9a3711327c9
LEZHONG-iMAC <lezhong-imac@LEZHONG-iMACdeiMac.local>
2017-04-26 16:02:47 +0800

    Initial Commit

---
+++ KouJiaoLaoHan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KouJiaoLaoHan
+//
+//  Created by LEZHONG-iMAC on 2017/4/26.
+//  Copyright © 2017年 LEZHONG-iMAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KouJiaoLaoHan/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KouJiaoLaoHan
+//
+//  Created by LEZHONG-iMAC on 2017/4/26.
+//  Copyright © 2017年 LEZHONG-iMAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
